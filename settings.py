3c3a44b406bfb9bd32e13fccd42232fb7885806e
ZAHABI5 <abolfazlzahabi5@gmail.com>
2025-05-21 16:26:33 +0330

    Third version

--- settings.py
+++ settings.py
@@ -3,6 +3,6 @@ class Settings:
     def __init__(self):
         self.screen_width = 1500
         self.screen_height = 800
-        self.bg_color = (200, 150, 150)
+        self.bg_color = (150, 200, 120)
         self.ship_speed = 1
         self.ship2_speed = 1
