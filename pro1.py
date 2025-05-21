0f5dc77fbf680a7f98fc5f78ede315e9ef22033d
ZAHABI5 <abolfazlzahabi5@gmail.com>
2025-05-21 16:18:11 +0330

    Third

--- pro1.py
+++ pro1.py
@@ -1,65 +1,89 @@
 import sys
 import pygame
-from settings import Settings
+from settings import Settings  
 from ship import Ship
+from ship2 import Ship2
 
 class AlienInvasion:
     def __init__(self):
         pygame.init()
         self.settings = Settings()
-        self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
-        self.settings.screen_width = self.screen.get_rect().width
-        self.settings.screen_height = self.screen.get_rect().height
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height)
+        )
         pygame.display.set_caption("Alien Invasion")
 
+        self.gameActive = True
         self.ship = Ship(self)
-        self.ship2 = Ship(self)
+        self.ship2 = Ship2(self)
+
+        self.bg_color = self.settings.bg_color
 
     def run_game(self):
         while True:
             self._check_events()
-            self.ship1.update()
-            self.ship2.update()
+            if self.gameActive:
+                self.ship.update()
+                self.ship2.update()
             self._update_screen()
+            pygame.display.flip()
 
     def _check_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                pygame.quit()
                 sys.exit()
+
             elif event.type == pygame.KEYDOWN:
                 self._check_keydown_events(event)
+
             elif event.type == pygame.KEYUP:
                 self._check_keyup_events(event)
 
     def _check_keydown_events(self, event):
-        if event.key == pygame.K_RIGHT:
-            self.ship1.moving_right = True
-        elif event.key == pygame.K_LEFT:
-            self.ship1.moving_left = True
-        elif event.key == pygame.K_d:
-            self.ship2.moving_right = True
+        
+        if event.key == pygame.K_w:
+            self.ship2.moving_up = True
         elif event.key == pygame.K_a:
             self.ship2.moving_left = True
-        elif event.key == pygame.K_q:
-            pygame.quit()
-            sys.exit()
+        elif event.key == pygame.K_s:
+            self.ship2.moving_down = True
+        elif event.key == pygame.K_d:
+            self.ship2.moving_right = True
+
+        elif event.key == pygame.K_UP:
+            self.ship.moving_up = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+        elif event.key == pygame.K_DOWN:
+            self.ship.moving_down = True
+        elif event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+
 
     def _check_keyup_events(self, event):
-        if event.key == pygame.K_RIGHT:
-            self.ship1.moving_right = False
-        elif event.key == pygame.K_LEFT:
-            self.ship1.moving_left = False
-        elif event.key == pygame.K_d:
-            self.ship2.moving_right = False
+        if event.key == pygame.K_w:
+            self.ship2.moving_up = False
         elif event.key == pygame.K_a:
             self.ship2.moving_left = False
+        elif event.key == pygame.K_s:
+            self.ship2.moving_down = False
+        elif event.key == pygame.K_d:
+            self.ship2.moving_right = False
+
+
+        elif event.key == pygame.K_UP:
+            self.ship.moving_up = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
+        elif event.key == pygame.K_DOWN:
+            self.ship.moving_down = False
+        elif event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
 
     def _update_screen(self):
-        self.screen.fill(self.settings.bg_color)
-        self.ship1.blitme()
+        self.screen.fill(self.bg_color)
+        self.ship.blitme()
         self.ship2.blitme()
-        pygame.display.flip()
 
 if __name__ == '__main__':
     ai = AlienInvasion()
--- settings.py
+++ settings.py
@@ -3,4 +3,6 @@ class Settings:
     def __init__(self):
         self.screen_width = 1500
         self.screen_height = 800
-        self.bg_color = (100, 150, 120)
+        self.bg_color = (200, 150, 150)
+        self.ship_speed = 1
+        self.ship2_speed = 1
