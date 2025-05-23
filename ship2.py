b7b3183c8132e99c9a52be774b5f4eb4fea9fd91
ZAHABI5 <abolfazlzahabi5@gmail.com>
2025-05-23 12:00:01 +0330

    Finall Version

--- ship2.py
+++ ship2.py
@@ -20,25 +20,26 @@ class Ship2:
         self.screen.blit(self.image, self.rect)
 
     def update(self, other_ship):
-         new_rect = self.rect.copy()
-
-         if self.moving_right and self.rect.right < self.screen_rect.right:
-             new_rect.x += self.settings.ship_speed
-         if self.moving_left and self.rect.left > 0:
-             new_rect.x -= self.settings.ship_speed
-         if self.moving_up and self.rect.top > 0:
-             new_rect.y -= self.settings.ship_speed
-         if self.moving_down and self.rect.bottom < self.screen_rect.bottom:
-             new_rect.y += self.settings.ship_speed
-
-         for wall in self.ai_game.walls:
+        new_rect = self.rect.copy()
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            new_rect.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            new_rect.x -= self.settings.ship_speed
+        if self.moving_up and self.rect.top > 0:
+            new_rect.y -= self.settings.ship_speed
+        if self.moving_down and self.rect.bottom < self.screen_rect.bottom:
+            new_rect.y += self.settings.ship_speed
+        scoreboard1_rect = pygame.Rect(-40, 0, 150, 60)
+        scoreboard2_rect = pygame.Rect(1380, 0, 150, 60)
+        if new_rect.colliderect(scoreboard1_rect) or new_rect.colliderect(scoreboard2_rect):
+            return
+        for wall in self.ai_game.walls:
             if new_rect.colliderect(wall.rect):
-                return  
+                return
+        if new_rect.colliderect(other_ship.rect):
+            return
 
-
-         if new_rect.colliderect(other_ship.rect):
-            return 
-         self.rect = new_rect
-         self.x = float(self.rect.x)
-         self.y = float(self.rect.y)
+        self.rect = new_rect
+        self.x = float(self.rect.x)
+        self.y = float(self.rect.y)
 
